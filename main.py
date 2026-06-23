96f1fcc54f32425c6412b27a5843cd9c92b2f238
Gurthog <226201902+Gurthog@users.noreply.github.com>
2026-06-23 10:29:58 -0600

    auto size window

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ from window import Window
 
 
 def main():
-    win = Window(800, 600)
-    maze = Maze(win, 15, 10, 45)
+    COLS = 15
+    ROWS = 10
+    CELL_SIZE = 45
+
+    win = Window(COLS * CELL_SIZE, ROWS * CELL_SIZE)
+    maze = Maze(win, COLS, ROWS, CELL_SIZE)
     start = maze.cells[2][2]
     end = maze.cells[8][8]
     start.draw_move(end)
